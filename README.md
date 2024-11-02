29e465d385de41d90b210edd02fb4c5bb5106d61
Joshua Reed <11220408+jreed1701@users.noreply.github.com>
2024-11-02 16:50:28 -0600

    Add sript and readme.

---
+++ README.md
@@ -0,0 +1,16 @@
+# TorNet Data Downloader
+
+This repo's sole purpose is to download the data called out in the tornet repo:
+
+https://github.com/mit-ll/tornet?tab=readme-ov-file#downloading-the-data
+
+# Pre-requisites
+
+Have python on your system.
+
+# Usage
+
+python -m venv venv
+. ./venv/bin/activate
+pip install -r requirements.txt
+./download.sh
---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+zenodo_get https://doi.org/10.5281/zenodo.12636522
+zenodo_get https://doi.org/10.5281/zenodo.12637032
+zenodo_get https://doi.org/10.5281/zenodo.12655151
+zenodo_get https://doi.org/10.5281/zenodo.12655179
+zenodo_get https://doi.org/10.5281/zenodo.12655183
+zenodo_get https://doi.org/10.5281/zenodo.12655187
+zenodo_get https://doi.org/10.5281/zenodo.12655716
+zenodo_get https://doi.org/10.5281/zenodo.12655717
+zenodo_get https://doi.org/10.5281/zenodo.12655718
+zenodo_get https://doi.org/10.5281/zenodo.12655719
